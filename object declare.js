a24e2f78b95286274ab4489a3ce8548f091babb9
mahmudul-hasan <mhmahmudul1211@gmail.com>
2024-09-13 23:22:10 +0600

    first commit

---
+++ object declare.js
@@ -0,0 +1,16 @@
+const mahmudul = {
+    fullName: 'mahmudul hasan'
+    : 221902172;
+    institute: 'green university of bangladesh';
+    degree: {
+        ssc: true;
+        hsc: true;
+        psc: true;
+        jsc: true;
+        Bsc: {
+            year: 2022 - 2035;
+        }
+    }
+    // Msc: false;
+}
+console.log(mahmudul);
